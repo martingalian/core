ce0668cac02011f8b1ddd49a523883e88c989165
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-10-13 15:32:19 +0200

    wip

---
+++ README.md
@@ -0,0 +1,1 @@
+# Martingalian Core
---
+++ src/CoreServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Martingalian\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
